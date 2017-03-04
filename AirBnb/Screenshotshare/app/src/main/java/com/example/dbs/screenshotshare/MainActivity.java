4fef8b615451ed35b07b225ccccb15159f4df1f8
isrinath <srinath.adepu9@gmail.com>
2017-03-04 20:29:49 +0800

    update

--- AirBnb/Screenshotshare/app/src/main/java/com/example/dbs/screenshotshare/MainActivity.java
+++ AirBnb/Screenshotshare/app/src/main/java/com/example/dbs/screenshotshare/MainActivity.java
@@ -3,20 +3,32 @@ package com.example.dbs.screenshotshare;
 import android.app.Activity;
 import android.app.ActivityManager;
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.os.Environment;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Toast;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+import static com.example.dbs.screenshotshare.MainActivity.ScreenShotUtil.getScreenShot;
 
+public class MainActivity extends AppCompatActivity {
+    private Context context;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        detectScreenShotService(this);
+     //   detectScreenShotService(this);
+
+        context = this;
+        View rootView = getWindow().getDecorView().findViewById(android.R.id.content);
+        Bitmap bitmap= ScreenShotUtil.getScreenShot(rootView);
+        ScreenShotUtil.store(bitmap,"test");
     }
 
     public void detectScreenShotService(final Activity activity) {
@@ -40,4 +52,39 @@ public class MainActivity extends AppCompatActivity {
         }, delay);
     }
 
+
+    /**
+     * Created by dbs on 4/3/17.
+     */
+
+    public static class ScreenShotUtil {
+        public static Bitmap getScreenShot(View view) {
+            View screenView = view.getRootView();
+            if(screenView != null){
+                screenView.setDrawingCacheEnabled(true);
+                Bitmap bitmap = Bitmap.createBitmap(screenView.getDrawingCache());
+                screenView.setDrawingCacheEnabled(false);
+                return bitmap;
+            }else{
+                return null;
+            }
+
+        }
+
+        public static void store(Bitmap bm, String fileName){
+            final String dirPath = Environment.getExternalStorageDirectory().getAbsolutePath() + "/Screenshots";
+            File dir = new File(dirPath);
+            if(!dir.exists())
+                dir.mkdirs();
+            File file = new File(dirPath, fileName);
+            try {
+                FileOutputStream fOut = new FileOutputStream(file);
+                bm.compress(Bitmap.CompressFormat.PNG, 85, fOut);
+                fOut.flush();
+                fOut.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
